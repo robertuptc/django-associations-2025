6d542753b5f08a71ef47884f133ad9807d1f92e6
robertuptc <robertuptc@gmail.com>
2025-05-16 14:01:57 -0400

    blog assignment finished

--- medium/blog/models.py
+++ medium/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user = models.CharField(max_length=255, null=True, blank=True)
+
+class Post(models.Model):
+    post = models.CharField(max_length=255, null=True, blank=True)
+    author = models.ForeignKey("User", on_delete=models.CASCADE, related_name='posts')
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=255, null=True, blank=True)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey("User", on_delete=models.CASCADE, related_name="comments")
