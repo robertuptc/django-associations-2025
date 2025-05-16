d6ab228c19077f204dafdb545081acd889c7820a
robertuptc <robertuptc@gmail.com>
2025-05-16 19:54:34 -0400

    amazon assignment finished

--- amazon/store/models.py
+++ amazon/store/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Shop(models.Model):
+    owner = models.OneToOneField("User", on_delete=models.CASCADE, related_name='shop')
+
+class Product(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name='products')
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='reviews')
+
+class User(models.Model):
+    reviewed_products = models.ManyToManyField(Product, related_name='user')
